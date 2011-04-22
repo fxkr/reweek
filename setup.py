787e5f85ce0492fab4a22b9c5e1637bd80b5e4c0
Felix Kaiser <felix.kaiser@fxkr.net>
2011-04-22 14:59:23 +0200

    Version 0.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='reweek',
-  version='0.1',
+  version='0.1.1',
   description='adds the weekday to all yyyy-mm-dd strings in a text',
   author='Felix Kaiser',
   author_email='felix.kaiser@fxkr.net',
